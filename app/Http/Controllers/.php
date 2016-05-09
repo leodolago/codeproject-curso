ac9c3ae1c714f69ad50b05dbaa09841a859b1a5c
Leonardo Lago <leodolago@gmail.com>
2016-05-09 13:40:39 -0300

    Ok primeiro commit aqui

---
+++ app/Http/Controllers/.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use CodeProject\Client;
+use Illuminate\Http\Request;
+
+use CodeProject\Http\Requests;
+use CodeProject\Http\Controllers\Controller;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return \CodeProject\Client::all();
+    }
+    
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Client::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Client::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    public function update(Request $request, $id )
+    {
+
+        return Client::find($id)->updated($request->all());
+
+    }
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+
+        try
+        {
+            Client::find($id)->delete();
+        return response()->json(['...'])
+    }
+        catch (\Exception $e)
+        {
+            return response()->json(['...'])
+    }
+    }
+}
