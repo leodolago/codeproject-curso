db38cd0082c3c6d60f6215e388ace88ab81b16f2
Leonardo Lago <leodolago@gmail.com>
2016-05-18 15:36:23 -0300

    Criando o primeiro Repository

---
+++ app/Repositories/ClientRepositoryEloquent.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: leodolago
+ * Date: 18/05/2016
+ * Time: 15:19
+ */
+
+namespace CodeProject\Repositories;
+
+
+use CodeProject\Entities\Client;
+use Prettus\Repository\Eloquent\BaseRepository;
+
+class ClientRepositoryEloquent extends BaseRepository
+{
+
+    public function model()
+    {
+        return Client::class;
+    }
+}
